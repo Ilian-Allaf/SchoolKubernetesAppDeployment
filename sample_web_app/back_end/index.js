8a9c09fd2b431e7d308e70345eb503de17277942
TanguyLichau <tanguylichau@gmail.com>
2023-02-28 19:56:18 +0100

    Changes

--- sample_web_app/back_end/index.js
+++ sample_web_app/back_end/index.js
@@ -5,27 +5,24 @@ const mongoose = require("mongoose");
 const port = 3000;
 
 app.use(cors());
-app.listen(port, () =>
-  console.log(`Example app listening at http://localhost:${port}`)
-);
 app.get("/", controllerGetAllLocations);
 
-const Cat = mongoose.model('Cat', { name: String });
+const Cat = mongoose.model("Cat", { name: String });
 
-async function main(){
-    mongoose.connect('mongodb://localhost:27017');
-    const kitty = new Cat({ name: 'Zildjian' });
-    kitty.save().then(() => console.log('meow'));
+async function main() {
+  mongoose.set("strictQuery", true);
+  await mongoose.connect("mongodb://localhost:27017");
+  console.log("Connected to MongoDB");
+  const kitty = new Cat({ name: "Zildjian" });
+  kitty.save().then(() => console.log("meow"));
+  app.listen(port, () =>
+    console.log(`Example app listening at http://localhost:${port}`)
+  );
 }
 
 async function controllerGetAllLocations(req, res) {
-    const cats = await Cat.find();
-    return res.status(200).send(cats);
+  const cats = await Cat.find();
+  return res.status(200).send(cats);
 }
 
 main();
-
-
-
-
-
