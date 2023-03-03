062feb7a6b9e9d6e4ed0103f6b2c2837b29fcae0
TanguyLichau <tanguylichau@gmail.com>
2023-03-03 13:23:52 +0100

    Changes

--- createApp.bat
+++ createApp.bat
@@ -1,7 +1,7 @@
 kubectl apply -f deployments/database_deployment.yaml
 kubectl apply -f deployments/back_deployment.yaml
-kubectl apply -f deployments/test.yaml
+::kubectl apply -f deployments/test.yaml
 kubectl apply -f services/database-service.yaml
 kubectl apply -f services/back-service.yaml
-kubectl apply -f services/front-service.yaml
-minikube service frontend-service -n web-app --url
+::kubectl apply -f services/front-service.yaml
+minikube service backend-service -n web-app --url
